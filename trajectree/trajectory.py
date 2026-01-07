f55f34cbcc04c29fbda9e70be95ee2c8f9f3e035
ANSH SINGAL <anshsingal1999@gmail.com>
2026-01-07 17:55:48 -0600

    Initial debug. Validate.

--- trajectree/trajectory.py
+++ trajectree/trajectory.py
@@ -72,12 +72,13 @@ class trajectory_evaluator():
 
         # Visualizing the Trajectree:
         self.graph = Tree()
+        self.graph.create_node(str(self.traversed_nodes), str(self.traversed_nodes))
         # self.graph.create_node(str(self.traversed_nodes), str(self.traversed_nodes))  # root node
 
         # for debugging only:
-        self.cache_hit = 0
-        self.cache_miss = 0
-        self.cache_partial_hit = 0
+        # self.cache_hit = 0
+        # self.cache_miss = 0
+        # self.cache_partial_hit = 0
 
 
     def apply_kraus(self, psi, kraus_MPOs, error_tolerance, normalize = True):
@@ -119,42 +120,15 @@ class trajectory_evaluator():
         return trajectories, trajectory_weights
 
 
-    def cache_trajectree_node(self, trajectory_weights, trajectories):
-        sorted_indices = np.argsort(trajectory_weights)
-
-        # print("trajectory_weights", trajectory_weights)
-
-        if len(self.cache_heap) < self.max_cache_nodes or self.max_cache_nodes == -1:
-            push_node = True
-            cached_trajectory_indices = sorted_indices[-self.cache_size:]
-        else:
-            push_node = False
-            cached_trajectory_indices = []
-
-        cached_trajectories = np.array(trajectories)[cached_trajectory_indices]
-
-        new_node = trajectree_node(trajectory_weights, cached_trajectories, cached_trajectory_indices)
-        self.trajectree[len(self.traversed_nodes)][self.traversed_nodes] = new_node
-        if len(self.traversed_nodes) == 0:
-            self.graph.create_node(str(self.traversed_nodes), str(self.traversed_nodes))  # root node
-        else:
-            self.graph.create_node(str(self.traversed_nodes), str(self.traversed_nodes), parent = str(self.traversed_nodes[:-1]))
-
-        self.last_cached_node = new_node
-
-        if push_node:
-            heapq.heappush(self.cache_heap, new_node)
-
-        return cached_trajectory_indices
-
 
     def discover_trajectree_node(self, psi, kraus_MPOs, error_tolerance, normalize = True, selected_trajectory_index = None):
         # read_quantum_state(psi, N=3)
         trajectories, trajectory_weights = self.apply_kraus(psi, kraus_MPOs, error_tolerance, normalize)
 
-        cached_trajectory_indices = self.cache_trajectree_node(trajectory_weights, trajectories) # cached_trajectory_indices is returned only for debugging. 
-
-        # print("trajectory weights:", trajectory_weights)
+        # Instead of caching trajectree node right here, we save the node with all the trajectories and weights, and later decide which trajectories to cache after any closed operations. 
+        # cached_trajectory_indices = self.cache_trajectree_node(trajectory_weights, trajectories) # cached_trajectory_indices is returned only for debugging. 
+        new_node = trajectree_node(trajectory_weights, trajectories, np.arange(len(trajectory_weights)))
+        self.trajectree[len(self.traversed_nodes)][self.traversed_nodes] = new_node
 
         if selected_trajectory_index == None:
             try:
@@ -170,50 +144,46 @@ class trajectory_evaluator():
         return trajectories[selected_trajectory_index]
 
 
-    def query_trajectree(self, psi, kraus_MPOs, error_tolerance, selected_trajectory_index = None, normalize = True):
-        self.skip_unitary = False
-        self.cache_unitary = False
-        # print("entering trajectree magnitude:", psi.H @ psi)
-        
+    def query_trajectree(self, psi, kraus_MPOs, error_tolerance, selected_trajectory_index = None, normalize = True):      
         if self.cache_size == 0:
             trajectories, trajectory_weights = self.apply_kraus(psi, kraus_MPOs, error_tolerance, normalize)
             selected_trajectory_index = np.random.choice(a = len(trajectory_weights), p = trajectory_weights/sum(trajectory_weights))
             logger.info("selected index while not caching was: %d", selected_trajectory_index)
-            # psi = tensor_network_apply_op_vec(self.kraus_channels[len(self.traversed_nodes)].get_MPOs()[selected_trajectory_index], psi, compress=True, contract = True, cutoff = error_tolerance)
             self.traversed_nodes = self.traversed_nodes + (selected_trajectory_index,)
             return trajectories[selected_trajectory_index]
 
-        if self.traversed_nodes in self.trajectree[len(self.traversed_nodes)]: # Check if the dictionary at level where the traversal is now, i.e., len(self.traversed_nodes)
-                                                                               # has the path that the present traversal has taken. 
+        # Check if the node has been discovered previously.
+        if self.traversed_nodes in self.trajectree[len(self.traversed_nodes)]: 
+            self.new_node_discovered = False # If the node has been found, we do not cache the unitary. The unitary is either already cached or we don't need to cache it at all.
+
             node = self.trajectree[len(self.traversed_nodes)][self.traversed_nodes] # If found, index that node into the node object to call the probabilities and trajectories cached inside it.
             if selected_trajectory_index == None:
-                # print("cached weights:", node.weights, "at:", self.traversed_nodes)
                 selected_trajectory_index = np.random.choice(a = len(node.weights), p = node.weights/sum(node.weights)) # The cached nodes have all the weights, but not all the trajectories cache. So, we can select
                 logger.info("selected index while discovered node found was: %d", selected_trajectory_index)                      # what trajecory our traversal takes and later see if the actual trajectory has been cached or needs to be retrieved. 
-                # print("cached weights:", node.weights, "at:", self.traversed_nodes, "selected trajectory:", selected_trajectory_index)
-            self.cache_unitary = False # If the node has been found, we do not cache the unitary. The unitary is either already cached or we don't need to cache it at all.
 
-            if selected_trajectory_index in node.trajectory_indices: # See if the selected trajectory's MPS has been cached or not. 
-                self.skip_unitary = True # If we're skipping the unitary entirely, it just does not matter whether we cache the unitary or not.
-                self.cache_hit += 1
+            # Check if cached trajectory is present:
+            if selected_trajectory_index in node.trajectory_indices:  
+                self.cached_trajectory_found = True # If we're skipping the unitary entirely, it just does not matter whether we cache the unitary or not.
                 psi = node.trajectories[np.where(node.trajectory_indices == selected_trajectory_index)[0][0]]
+                # self.cache_hit += 1
             else: 
-                self.skip_unitary = False # If the trajectory has not been cached, we will have to apply the unitary to it.
-                self.cache_partial_hit += 1
-                psi = tensor_network_apply_op_vec(self.kraus_channels[len(self.traversed_nodes)].get_MPOs()[selected_trajectory_index], psi, compress=True, contract = True, cutoff = error_tolerance) # If not, simply calculate that trajectory. 
-                                                                                                                                                                                             # You don't need to cache it since we have already cached what we had to.  
-                # print("cache partial hit. new state:" )
-                # read_quantum_state(psi, N=3)
+                self.cached_trajectory_found = False # If the trajectory has not been cached, we will have to apply the unitary to it.
+                psi = tensor_network_apply_op_vec(self.kraus_channels[len(self.traversed_nodes)].get_MPOs()[selected_trajectory_index], psi, compress=True, contract = True, cutoff = error_tolerance) # If not, simply calculate that trajectory. You don't need to cache it since we have already cached what we had to.  
                 if normalize:
                     # After this, the trajectory is always normalized. 
                     psi /= np.sqrt(node.weights[selected_trajectory_index])
+                if node in self.cache_heap:
+                    node.trajectories.append(psi)
+                    node.trajectory_indices = np.append(node.trajectory_indices, selected_trajectory_index)
+                # self.cache_partial_hit += 1
+
             self.traversed_nodes = self.traversed_nodes + (selected_trajectory_index,)
 
             # Here, we check if the node needs to be cached or not. 
             node.accesses += 1
             if len(node.trajectory_indices) == 0: # If the node has been discovered but not cached
                 if node.accesses > self.cache_heap[0].accesses: # the new node has been accesses more often than the top of the heap, so, we can replace the top of the heap with the new node.
-                    # print("in here!!!")
+                    # You'll need to add a functionality here to start caching all the trajectories which have been added to the heap. 
                     old_node = heapq.heapreplace(self.cache_heap, node)
                     del old_node.trajectories
                     old_node.trajectories = [] 
@@ -222,18 +192,10 @@ class trajectory_evaluator():
 
 
         else: # If the node has not been discovered, we'll have to find all weights and cache the results.
-            # print("exploring new node at:", self.traversed_nodes)
-            # read_quantum_state(psi, N=3) 
-            self.skip_unitary = False
-            self.cache_unitary = True
-            self.cache_miss += 1
+            self.new_node_discovered = True
+            self.cached_trajectory_found = False
+            # self.cache_miss += 1
             psi = self.discover_trajectree_node(psi, kraus_MPOs, error_tolerance, normalize, selected_trajectory_index = selected_trajectory_index)
-
-        # print("exitting trajectree magnitude:", psi.H @ psi)
-        # if psi == None:
-        #     # raise Exception(f"traversed nodes: {self.traversed_nodes} trajectory_weights: {trajectory_weights} invalid")
-        #     self.graph.show()
-        #     raise Exception(f"Psi is None in query trajectree. traversed nodes: {self.traversed_nodes}")
         return psi
 
     def apply_unitary_MPOs(self, psi, unitary_MPOs, error_tolerance):
@@ -251,65 +213,74 @@ class trajectory_evaluator():
             dm += psi_new_dense @ psi_new_dense.conj().T
         return dm
 
-    def update_cached_node(self, unitary_MPOs, last_cached_node, error_tolerance):
-        for kraus_idx in range(len(last_cached_node.trajectories)):
-            if last_cached_node.trajectories[kraus_idx] is not None:
-                last_cached_node.trajectories[kraus_idx] = self.apply_unitary_MPOs(last_cached_node.trajectories[kraus_idx], unitary_MPOs, error_tolerance)
-                if np.real(last_cached_node.trajectories[kraus_idx].H @ last_cached_node.trajectories[kraus_idx]) < 1e-25:
-                    last_cached_node.trajectories[kraus_idx] = None
+    def update_cached_node(self, unitary_MPOs, last_discovered_node, error_tolerance):
+        for kraus_idx in range(len(last_discovered_node.trajectories)):
 
+            if last_discovered_node.trajectories[kraus_idx] is not None:
+                last_discovered_node.trajectories[kraus_idx] = self.apply_unitary_MPOs(last_discovered_node.trajectories[kraus_idx], unitary_MPOs, error_tolerance)
+                last_discovered_node.weights[kraus_idx] = np.real(last_discovered_node.trajectories[kraus_idx].H @ last_discovered_node.trajectories[kraus_idx])
+                
+                if last_discovered_node.weights[kraus_idx] < 1e-25:
+                    last_discovered_node.trajectories[kraus_idx] = None
+                    last_discovered_node.weights[kraus_idx] = 0
+
+    def cache_trajectree_node(self):
+        last_discovered_node = self.trajectree[len(self.traversed_nodes)-1][self.traversed_nodes[:-1]]
+
+        sorted_indices = np.argsort(last_discovered_node.weights)
+
+        if len(self.cache_heap) < self.max_cache_nodes or self.max_cache_nodes == -1:
+            cached_trajectory_indices = sorted_indices[-self.cache_size:]
+            cached_trajectories = np.array(last_discovered_node.trajectories)[cached_trajectory_indices]
+            
+            last_discovered_node.trajectories = cached_trajectories
+            last_discovered_node.trajectory_indices = cached_trajectory_indices
+            heapq.heappush(self.cache_heap, last_discovered_node)
+        else:
+            last_discovered_node.trajectories = np.array([])
+            last_discovered_node.trajectory_indices = np.array([])
+
+        self.graph.create_node(str(self.traversed_nodes), str(self.traversed_nodes), parent = str(self.traversed_nodes[:-1]))
 
     # NOTE: USE NORMALIZE = TRUE FOR TRAJECTORY SIMULATIONS. USE NORMALIZE = FALSE FOR DENSITY MATRIX CALCULATIONS.
     def perform_simulation(self, psi, error_tolerance, trajectree_indices = None, normalize = True):
         self.traversed_nodes = ()
-        self.skip_unitary = False
-        self.cache_unitary = False
+        self.cached_trajectory_found = False
+        self.new_node_discovered = False
+
         for quantum_channel in self.quantum_channels:
-            # print("operation:", quantum_channel.name, "formalism:", quantum_channel.formalism, "traversed nodes:", self.traversed_nodes)
             if quantum_channel.formalism == 'kraus':
+                if self.new_node_discovered: # If we had previously discovered a new node, cache the previously discovered node. 
+                    self.cache_trajectree_node()  
+                
+                # Reset the flags for the next kraus operation. 
+                self.cached_trajectory_found = False
+                self.new_node_discovered = False
+
                 kraus_MPOs = quantum_channel.get_MPOs()
-                # print("kraus op:", quantum_channel.name, "number of kraus ops:", len(kraus_MPOs))
-                # print("before kraus ops:")
-                # read_quantum_state(psi, N=3)
                 if not trajectree_indices == None: # If the list of trajectoery indices is provided, we will use that to traverse the trajectree. The random number generators will not be used.
                     psi = self.query_trajectree(psi, kraus_MPOs, error_tolerance, trajectree_indices.pop(0), normalize)
                 else: # In this branch, you actually select the trajectory redomly and perform realistic simulations. 
-                    # read_quantum_state(psi, N=3)
                     psi = self.query_trajectree(psi, kraus_MPOs, error_tolerance, normalize = normalize)
-                # print("after kraus ops:")
-                # read_quantum_state(psi, N=3)
 
-            elif quantum_channel.formalism == 'closed' and not self.skip_unitary:
-                # print("closed op:", quantum_channel.name)
+            elif quantum_channel.formalism == 'closed' and not self.cached_trajectory_found:
                 unitary_MPOs = quantum_channel.get_MPOs()
                                 
                 if self.cache_size == 0: # If we aren't aching the trajectories at all, simply apply the unitary MPOs to the state.
                     psi = self.apply_unitary_MPOs(psi, unitary_MPOs, error_tolerance)
                     continue
 
-                last_cached_node = self.trajectree[len(self.traversed_nodes)-1][self.traversed_nodes[:-1]]
-                
-                if self.cache_unitary:
-                    self.update_cached_node(unitary_MPOs, last_cached_node, error_tolerance)
-
-                # This is where we are checking if the psi is cached or not. If it is, simply use the last cached node 
-                # node to update psi. If not, apply the unitary MPOs to psi.
-                traj_idx = np.where(last_cached_node.trajectory_indices == self.traversed_nodes[-1])    
-                if traj_idx[0].size > 0:
-                    psi = last_cached_node.trajectories[traj_idx[0][0]]
+                if self.new_node_discovered:
+                    last_discovered_node = self.trajectree[len(self.traversed_nodes)-1][self.traversed_nodes[:-1]] # Here, we have len(self.traversed_nodes)-1 because we want the last level explored in the tree in this run
+                                                                                                           # and, we have self.traversed_nodes[:-1] because self.traversed_nodes corresponds to the node after applying the kruas operation. 
+                    self.update_cached_node(unitary_MPOs, last_discovered_node, error_tolerance)
+                    traj_idx = np.where(last_discovered_node.trajectory_indices == self.traversed_nodes[-1])    
+                    psi = last_discovered_node.trajectories[traj_idx[0][0]]
                 else:
                     psi = self.apply_unitary_MPOs(psi, unitary_MPOs, error_tolerance)
                     if np.real(psi.H @ psi) < 1e-25:
                         psi = None
-            
-            else:
-                # print("unitary skipped:", self.traversed_nodes)
-                pass
-            if psi == None:
-                # print("Nothing returned")
-                return 0
-                # raise Exception("Psi is None")
-            # read_quantum_state(psi, N=6)
-            # print("next operation:")
 
+            if psi == None:
+                return 0
         return psi
